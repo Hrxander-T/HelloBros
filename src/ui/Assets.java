91e620b6de56ede65431ed6806a5d00b2ae265e4
Hrxander <hrtahsinhossain@gmail.com>
2026-05-15 21:07:02 +0600

    refactor: replace direct ImageIcon instants with load and scale methods

--- src/ui/Assets.java
+++ src/ui/Assets.java
@@ -4,12 +4,18 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Assets {
-    public static final ImageIcon DICE = new ImageIcon("assets/dice.png");
-    public static final ImageIcon SPEECH_BUBBLE = new ImageIcon(
-        new ImageIcon("assets/speech_bubble.png").getImage()
-            .getScaledInstance(24, 24, Image.SCALE_SMOOTH)
-    );
+    public static final ImageIcon DICE = load("assets/dice.png");
+    public static final ImageIcon SPEECH_BUBBLE = scale(load("assets/speech_bubble.png"), 24, 24);
+    public static final ImageIcon HOURGLASS = load("assets/hourglass.png");
+    public static final ImageIcon PAPERCLIP = load("assets/paperclip.png");
 
-    public static final ImageIcon HOURGLASS = new ImageIcon("assets/hourglass.png");
-    public static final ImageIcon PAPERCLIP = new ImageIcon("assets/paperclip.png");
+    private static ImageIcon load(String path) {
+        var url = Assets.class.getClassLoader().getResource(path);
+        System.out.println("Loading: " + path + " -> " + url);
+        return new ImageIcon(url);
+    }
+
+    private static ImageIcon scale(ImageIcon icon, int w, int h) {
+        return new ImageIcon(icon.getImage().getScaledInstance(w, h, Image.SCALE_SMOOTH));
+    }
 }
